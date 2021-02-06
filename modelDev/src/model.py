090e13a4aaf0e3bfc841ea3a0f1549b45fa5cf11
olivierSaintCyr <olivier@WhiteShadow.localdomain>
2021-02-05 22:29:37 -0500

    adding some neurons on the project

---
+++ modelDev/src/model.py
@@ -0,0 +1,37 @@
+import tensorflow as tf
+from tensorflow.keras.applications import ResNet50V2
+import keras
+#######################################
+config = tf.compat.v1.ConfigProto()   #
+config.gpu_options.allow_growth = True#
+tf.compat.v1.Session(config=config)   #
+#######################################
+
+class Model(object):
+    def __init__(self):
+        self.model = self.build_model()
+
+    def build_model(self):
+        resnet = ResNet50V2(
+            input_shape=(64, 64, 3),
+            weights="imagenet",
+            include_top=False,
+            pooling=max,
+            classifier_activation='relu'
+        )
+        
+        resnet.trainable = True
+        model = tf.keras.models.Sequential()
+
+        model.add(resnet)
+        model.add(keras.layers.GlobalMaxPooling2D())
+        
+        model.add(keras.layers.Dense(38, activation = 'sigmoid'))
+        model.compile(loss = 'categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+        return model
+    
+    def load_model(self, path):
+        self.model = keras.models.load_model(path)
+
+    def train_model(self, ds):
+        self.model.fit(ds)
