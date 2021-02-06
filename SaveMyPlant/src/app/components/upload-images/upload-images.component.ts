618d99fe26f8d44a5697016182a1aa59ec546309
julie-annej <julie-annej@users.noreply.github.com>
2021-02-06 01:50:43 -0500

    upload

--- SaveMyPlant/src/app/components/upload-images/upload-images.component.ts
+++ SaveMyPlant/src/app/components/upload-images/upload-images.component.ts
@@ -1,86 +1,51 @@
-// import { Component, ViewChild, ElementRef } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
 
-// @Component({
-//   selector: "upload-images",
-//   templateUrl: "./upload-images.html",
-//   styleUrls: ["./upload-images.scss"],
-// })
-// export class AppComponent {
-//   @ViewChild("fileDropRef", { static: false }) fileDropEl: ElementRef;
-//   files: any[] = [];
 
-//   /**
-//    * on file drop handler
-//    */
-//   onFileDropped($event) {
-//     this.prepareFilesList($event);
-//   }
+@Component({
+  selector: 'loading',
+  templateUrl: './loading.component.html',
+  styleUrls: ['./loading.component.css']
+})
+export class LoadingComponent implements OnInit {
+  selectedFile: File
 
-//   /**
-//    * handle file from browsing
-//    */
-//   fileBrowseHandler(files) {
-//     this.prepareFilesList(files);
-//   }
+  public imagePath;
+  imgURL: any;
+  public message: string;
+  constructor() { }
 
-//   /**
-//    * Delete file from files list
-//    * @param index (File index)
-//    */
-//   deleteFile(index: number) {
-//     if (this.files[index].progress < 100) {
-//       console.log("Upload in progress.");
-//       return;
-//     }
-//     this.files.splice(index, 1);
-//   }
+  ngOnInit(): void {
+  }
 
-//   /**
-//    * Simulate the upload process
-//    */
-//   uploadFilesSimulator(index: number) {
-//     setTimeout(() => {
-//       if (index === this.files.length) {
-//         return;
-//       } else {
-//         const progressInterval = setInterval(() => {
-//           if (this.files[index].progress === 100) {
-//             clearInterval(progressInterval);
-//             this.uploadFilesSimulator(index + 1);
-//           } else {
-//             this.files[index].progress += 5;
-//           }
-//         }, 200);
-//       }
-//     }, 1000);
-//   }
+  onFileChanged(event){
+    //this.selectedFile = event.target.files[0]
+    
+    const file = event.target.files[0];
+    this.preview(file);
+  }
+  onUpload(){
+    // Faire le lien avec le AI
 
-//   /**
-//    * Convert Files list to normal array list
-//    * @param files (Files List)
-//    */
-//   prepareFilesList(files: Array<any>) {
-//     for (const item of files) {
-//       item.progress = 0;
-//       this.files.push(item);
-//     }
-//     this.fileDropEl.nativeElement.value = "";
-//     this.uploadFilesSimulator(0);
-//   }
+  }
+  preview(files) {
+    if (files.length === 0)
+      return;
+ 
+    var mimeType = files[0].type;
+    if (mimeType.match(/image\/*/) == null) {
+      this.message = "Only images are supported.";
+      return;
+    }
+ 
+    var reader = new FileReader();
+    this.imagePath = files;
+    reader.readAsDataURL(files[0]); 
+    reader.onload = (_event) => { 
+      this.imgURL = reader.result; 
+    }
+  }
 
-//   /**
-//    * format bytes
-//    * @param bytes (File size in bytes)
-//    * @param decimals (Decimals point)
-//    */
-//   formatBytes(bytes, decimals = 2) {
-//     if (bytes === 0) {
-//       return "0 Bytes";
-//     }
-//     const k = 1024;
-//     const dm = decimals <= 0 ? 0 : decimals;
-//     const sizes = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
-//     const i = Math.floor(Math.log(bytes) / Math.log(k));
-//     return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + " " + sizes[i];
-//   }
-// }
+
+
+}
+
